6e0a8407958271c29d4358d98d77d42c46d79eca
hopea114y <serhioalfa@mail.ru>
2017-08-19 10:17:33 +0300

    Make a header (desktop and mobile version)

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,17 @@ $(".owl-carousel").owlCarousel({
 	items: 1,
 	smartSpeed: 700,
 })
+
+$(function() {
+	var link = $('.m-menu-link');
+	var close = $('.close-menu');
+	var menu = $('.m-menu');
+	link.on('click', function(event) {
+		event.preventDefault();
+		menu.toggleClass('m-menu__active');
+	})
+	close.on('click', function(event) {
+		event.preventDefault();
+		menu.toggleClass('m-menu__active');
+	})
+})
